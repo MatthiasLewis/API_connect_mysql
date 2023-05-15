5040e36995119a12d33c1a01e695b332f0df7b1a
MatthiasLewis <youtdbbac667@gmail.com>
2023-05-15 22:16:18 +0800

    update api.py

--- restful_user/api.py
+++ restful_user/api.py
@@ -1,13 +1,44 @@
 from flask import Flask
 from flask_restful import Api
-from resource.member import Member
+from resource.member import Member, SingleMember, Login
+from flask_jwt_extended import JWTManager
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+from flask_apispec.extension import FlaskApiSpec
 
-#Api繼承了Flask的函數跟屬性
 app = Flask(__name__)
 api = Api(app)
+
 app.config["DEBUG"] = True
+app.config["JWT_SECRET_KEY"] = "secret_key" #JWT token setting
+
+# Swagger setting
+app.config.update({
+    'APISPEC_SPEC': APISpec(
+        title='Awesome Project',
+        version='v1',
+        plugins=[MarshmallowPlugin()],
+        securityDefinitions={
+            "bearer": {
+                "type": "apiKey",
+                "in": "header",
+                "name": "Authorization",
+            }
+        },
+        openapi_version='2.0.0'
+    ),
+    'APISPEC_SWAGGER_URL': '/swagger/',  # URI to access API Doc JSON
+    'APISPEC_SWAGGER_UI_URL': '/swagger-ui/'  # URI to access UI of API Doc
+})
+docs = FlaskApiSpec(app)
 
 api.add_resource(Member, "/members")
+docs.register(Member)
+api.add_resource(SingleMember, "/member/<int:id>")
+docs.register(SingleMember)
+api.add_resource(Login, "/login")
+docs.register(Login)
 
 if __name__ == '__main__':
+    jwt = JWTManager().init_app(app)
     app.run(debug=True)
