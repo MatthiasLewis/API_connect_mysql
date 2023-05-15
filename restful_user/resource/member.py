96b19a96bc478522ff6f8e00f83e295bed7f7870
MatthiasLewis <youtdbbac667@gmail.com>
2023-05-15 22:17:44 +0800

    update member.py

--- restful_user/resource/member.py
+++ restful_user/resource/member.py
@@ -1,71 +1,189 @@
 import pymysql
-from flask_restful import Resource,reqparse
+from flask_apispec import MethodResource, marshal_with, doc, use_kwargs
 from flask import jsonify
+from flask_jwt_extended import create_access_token, jwt_required,get_jwt_identity
+from . import route_model
+from datetime import timedelta
 import os
-#©w∏qmysql.connect™∫®Á¶°°A®√¶^∂«db§Œcursor
+
 def db_init():
-    KEY =os.getenv("SQL_PD")
     db = pymysql.connect(
         host='localhost',
         user='admin1',
-        password=KEY,
+        password=os.getenv("SQL_PD"),
         port=3306,
         db='user'
     )
     cursor = db.cursor(pymysql.cursors.DictCursor)
     return db, cursor
 
-#©w∏q§@≠”ƒ~©”§FResource™∫class°A®√≥]get§Œpost®‚∫ÿ®Á¶°
-class Member(Resource):
-    def get(self):   #•Hrequest.get§Ë¶°®˙±ouser.member≥o≠”table™∫§∫Æe°A®√•HjsonÆÊ¶°ße≤{
+security_params = [{"bearer": []}]
+
+class Login(MethodResource):
+    @doc(description="login API", tags=['Login'])
+    @use_kwargs(route_model.LoginSchema, location="form")
+    @marshal_with(route_model.LoginResponse)
+    def post(self, **kwargs):
         db, cursor = db_init()
-        #≠Y®S¶≥•Hget§Ë¶°≠n®D¶^∂«´¸©w™∫name°A´h¶^∂«æ„≠”member table§∫Æe
-        parser = reqparse.RequestParser()  
-        parser.add_argument('name', type=str, location="args")
-        args = parser.parse_args()
+        account, password = kwargs["account"], kwargs["password"]
+        sql = f"SELECT * FROM user.member WHERE account = '{account}' AND password = '{password}';"
+        cursor.execute(sql)
+        user = cursor.fetchall()
+        db.close()
+
+        if user != ():
+            token = get_access_token(account,user[0]["role_id"])
+            data = {
+                "message": f"Welcome back {user[0]['name']}",
+                "token": token}
+            return data, 200
         
-        filter_name = args.get("name")
-        if filter_name is None:
-            sql = 'SELECT * FROM user.member;'
-        else:
-            sql = f"SELECT * FROM user.member WHERE name LIKE '%{filter_name}%';"
+        return jsonify({"message":"Account or password is wrong"})
+#ÂæûJWT‰∏≠Âèñ"user_claims"ÔºåËã•Ê≤íÊúâÊ¨äÈôêÊúÉÁõ¥Êé•403
+def set_http_methods(func):
+    def wrap(*args,**kwrags):
+        current_user = get_jwt_identity()
+        http_method = current_user["user_claims"]
+        if http_method=="GET" and func.__name__ != "get":
+            return jsonify({"message":"Ê≤íÊúâÊ¨äÈôê"})
+        elif http_method=="!DELETE" and func.__name__ =="delete":
+                return jsonify({"message":"Ê≤íÊúâÊ¨äÈôê"})
+        return func(*args,**kwrags)
+    return wrap
+#Âª∫Á´ãJWTtoken       
+def get_access_token(account,role_id):
+    http_method=None
+    if role_id==1:
+        http_method="GET"
+    elif role_id==2:
+        http_method="!DELETE"
+    elif role_id==3:
+        http_method="*"
+    token = create_access_token(
+        identity={"account": account,"user_claims":http_method},
+        expires_delta=timedelta(days=1)
+    )
+    return token
+
+class Member(MethodResource):
+    @doc(description="Get members", tags=['User'], security=security_params)
+    @use_kwargs(route_model.MemberGetSchema, location="query")
+    @marshal_with(route_model.MemberGetRes)
+    @jwt_required()
+    @set_http_methods
+    def get(self, name=None, limit=None , offset=0):
+        db, cursor = db_init()
+        sql = "SELECT * FROM user.member "
+        if name :
+            sql += f"where name LIKE '%{name}%' "
+        if limit :
+            sql += f"limit {limit} "    
+            sql += f"offset {offset}"
 
         cursor.execute(sql)
         users = cursor.fetchall()
         db.close()
-        return jsonify(users)
-    
-    # Create User
-    def post(self):#•Hrequest.post§Ë¶°∑sºWuser.member≥o≠”table™∫§∫Æe°A®√•HjsonÆÊ¶°ße≤{
+        return {"data":users}, 200
+    #,{"data": users}
+    @doc(description="create members", tags=['User'], security=security_params)
+    @use_kwargs(route_model.MemberPostSchema, location="form")
+    @marshal_with(route_model.MemberPostRes)
+    @jwt_required()
+    @set_http_methods
+    def post(self, **kwargs):
         db, cursor = db_init()
-        parser = reqparse.RequestParser() #®˙±opost™∫payload≠»
-        parser.add_argument('id', type=int, required=True, location="form")
-        parser.add_argument('name', type=str, required=True, location="form")
-        parser.add_argument('gender', type=str, required=True, location="form")
-        parser.add_argument('birth', type=str, required=True, location="form")
-        parser.add_argument('note', type=str, location="form")
-        args = parser.parse_args()
-        #±N®˙±o™∫payload≠»©Ò§Jdict
+        
         user = {
-            'id': args['id'],
-            'name': args['name'],
-            'gender': args['gender'],
-            'birth': args.get('birth') or '1900-01-01',
-            'note': args.get('note')
+            'id':kwargs['id'],
+            'name': kwargs['name'],
+            'gender': kwargs['gender'],
+            'birth': kwargs.get('birth') or '1900-01-01',
+            'note': kwargs.get('note'),
+            'account': kwargs['account'],
+            'password': kwargs['password'],
+            'role_id':kwargs['role_id']
         }
         sql = """
 
-        INSERT INTO `user`.`member` (`id`,`name`,`gender`,`birth`,`note`)
-        VALUES ('{}','{}','{}','{}','{}');
+        INSERT INTO `user`.`member` (`id`,`name`,`gender`,`birth`,`note`,`account`,`password`,`role_id`)
+        VALUES ('{}','{}','{}','{}','{}','{}','{}','{}');
 
-        """.format(user['id'],user['name'], user['gender'], user['birth'], user['note'])
+        """.format(user['id'],user['name'], user['gender'], user['birth'], user['note'], user['account'], user['password'],user['role_id'])
+        result = cursor.execute(sql)
+        message = "success" if result == 1 else "failure"
+        
+        db.commit()
+        db.close()      
+        return jsonify({"message": message})
+
+class SingleMember(MethodResource):
+    #Get single by id
+    @doc(description="get single members", tags=['User'], security=security_params)
+    @marshal_with(route_model.SingleMemberGetRes)
+    @jwt_required()
+    @set_http_methods
+    def get(self, id):
+        db, cursor = db_init()
+        sql = f"SELECT * FROM user.member WHERE id = '{id}';"
+        cursor.execute(sql)
+        users = cursor.fetchall()
+        db.close()
+        return {"data": users}, 200
+
+    @doc(description="update single members", tags=['User'], security=security_params)
+    @use_kwargs(route_model.SingleMemberPatchRes, location="form")
+    @marshal_with(route_model.SingleMemberPatcRes)
+    @jwt_required()
+    @set_http_methods
+    def patch(self, id, **kwargs):
+        db, cursor = db_init()
+        
+        user = {
+            'name': kwargs.get('name'),
+            'gender': kwargs.get('gender'),
+            'birth': kwargs.get('birth'),
+            'note': kwargs.get('note'),
+            'account': kwargs.get('account'),
+            'password': kwargs.get('password'),
+        }
+
+        query = []
+        for key, value in user.items():
+            if value is not None:
+                query.append(f"{key} = '{value}'")
+        query = ",".join(query)
+        '''
+        UPDATE table_name
+        SET column1=value1, column2=value2, column3=value3¬∑¬∑¬∑
+        WHERE some_column=some_value;
+
+        '''
+        sql = """
+            UPDATE user.member
+            SET {}
+            WHERE id = {};
+        """.format(query, id)
+
         result = cursor.execute(sql)
         message = "success" if result == 1 else "failure"
         db.commit()
         db.close()
-        #¶^∂«§@≠”json{"message": message}°A¶p™Gresult¨∞True°A´h¨∞success°A§œ§ß¨∞failure
+        
+        return jsonify({"message": message})
+
+    @doc(description="Delete single members", tags=['User'], security=security_params)
+    @marshal_with(route_model.SingleMemberDeleteRes, code=200)
+    @jwt_required()
+    @set_http_methods
+    def delete(self, id):
+        db, cursor = db_init()
+        sql = f'DELETE FROM `user`.`member` WHERE id = {id};'
+        result = cursor.execute(sql)
+        message = "success" if result == 1 else "failure"
+        db.commit()
+        db.close()
+
         return jsonify({"message": message})
 
 
-#DELETE
-sql = f'DELETE FROM `user`.`member` WHERE id = {id};'
+
